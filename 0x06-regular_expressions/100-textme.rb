477b5dca8ce3e132f7b3ee0527d00c506221c1a6
Noah172 <jsebastian.garcia1995@gmail.com>
2020-02-25 04:03:18 +0000

    something crazy

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-f = ARGV[0].scan(/from:([^\]]+)/).join
+fr = ARGV[0].scan(/from:([^\]]+)/).join
 t = ARGV[0].scan(/to:([^\]]+)/).join
 f =  ARGV[0].scan(/flags:([^\]]+)/).join
 
-puts f + "," + t + "," + f
+puts fr + "," + t + "," + f
